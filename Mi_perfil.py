d353627006fb94ffa56f53f271e9c3dc529fcc73
Jose Santos <jmsantose@alumno.uspg.edu.gt>
2026-02-22 22:12:37 -0600

    codigo con bug

---
+++ Mi_perfil.py
@@ -0,0 +1,12 @@
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+ciudad = input("Ingresa tu ciudad: ")
+hobby = input("Ingresa tu hobby: ")
+
+# Mostrar tarjeta de presentación
+
+print("\n--- Tarjeta de Presentación ---")
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
+print("Hobby:", hobby)
