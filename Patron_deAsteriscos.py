2aed12ee6d615556de85f3d45e7e2480be061484
Jose Santos <jmsantose@alumno.uspg.edu.gt>
2026-03-07 16:47:50 -0600

    Agrego ejercicio de asteriscos

--- Patron_deAsteriscos.py
+++ Patron_deAsteriscos.py
@@ -15,3 +15,13 @@ print()
 # Pirámide centrada
 for i in range(1, n + 1):
     print(" " * (n - i) + "*" * (2*i - 1))
+
+print()
+
+# Diamante
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2*i - 1))
+
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "*" * (2*i - 1))
+    
